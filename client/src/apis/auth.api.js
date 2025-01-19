166685c1606bc29e90ca7d75910b5881496685cc
Admodeuscz <work.doantv@gmail.com>
2025-01-19 23:05:14 +0700

    feat: add authentication and state management with React Query and Zustand

---
+++ client/src/apis/auth.api.js
@@ -0,0 +1,17 @@
+import http from '../utils/http'
+
+const prefix = '/auth'
+export const URL_LOGIN = `${prefix}/login`
+export const URL_REGISTER = `${prefix}/register`
+export const URL_LOGOUT = `${prefix}/logout`
+export const URL_REFRESH_TOKEN = `${prefix}/refresh-access-token`
+const authApi = {
+  login: async (data) => {
+    return await http.post(URL_LOGIN, data)
+  },
+  logout: async () => {
+    return await http.post(URL_LOGOUT)
+  }
+}
+
+export default authApi
--- client/src/layouts/AuthLayout/LeftSidebarMenu.js
+++ client/src/layouts/AuthLayout/LeftSidebarMenu.js
@@ -1,134 +1,199 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import { Nav, NavItem, NavLink, UncontrolledTooltip, Dropdown, DropdownItem, DropdownToggle, DropdownMenu } from "reactstrap";
-import classnames from "classnames";
-import { connect, useDispatch, useSelector } from "react-redux";
+import classnames from 'classnames'
+import React, { useState } from 'react'
+import { connect, useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledTooltip
+} from 'reactstrap'
 
-import { setActiveTab, changeLayoutMode } from "../../redux/actions";
+import { changeLayoutMode, setActiveTab } from '../../redux/actions'
 
 //Import Images
-import logo from "../../assets/images/logo-dark.webp"
-import avatar1 from "../../assets/images/users/avatar-1.jpg";
+import logo from '../../assets/images/logo-dark.webp'
+import avatar1 from '../../assets/images/users/avatar-1.jpg'
 
-import { createSelector } from 'reselect';
+import { useMutation } from '@tanstack/react-query'
+import { createSelector } from 'reselect'
+import authApi from '../../apis/auth.api'
+import { setStoreUser } from '../../store/useStoreUser'
 
 function LeftSidebarMenu(props) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const selectLayoutProperties = createSelector(
-        (state) => state.Layout,
-        (layout) => ({
-          layoutMode: layout.layoutMode,
-        })
-      );
-      
-      const { layoutMode } = useSelector(selectLayoutProperties);
-
-    const mode =
-        layoutMode === "dark"
-            ? "light"
-            : "dark";
-
-    const onChangeLayoutMode = (value) => {
-        if (changeLayoutMode) {
-            dispatch(changeLayoutMode(value));
-        }
+  const logoutMutation = useMutation({
+    mutationFn: authApi.logout,
+    onSuccess: () => {
+      setStoreUser({ profile: null, isLogged: false })
+      navigate('/login')
     }
+  })
 
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const [dropdownOpenMobile, setDropdownOpenMobile] = useState(false);
+  const selectLayoutProperties = createSelector(
+    (state) => state.Layout,
+    (layout) => ({
+      layoutMode: layout.layoutMode
+    })
+  )
 
-    const toggle = () => setDropdownOpen(!dropdownOpen);
-    const toggleMobile = () => setDropdownOpenMobile(!dropdownOpenMobile);
+  const { layoutMode } = useSelector(selectLayoutProperties)
 
-    const toggleTab = tab => {
-        props.setActiveTab(tab)
+  const mode = layoutMode === 'dark' ? 'light' : 'dark'
+
+  const onChangeLayoutMode = (value) => {
+    if (changeLayoutMode) {
+      dispatch(changeLayoutMode(value))
     }
+  }
 
-    const activeTab = props.activeTab;
-    return (
-        <React.Fragment>
-            <div className="side-menu flex-lg-column me-lg-1">
-                {/* LOGO */}
-                <div className="navbar-brand-box">
-                    <Link to="/" className="logo logo-dark">
-                        <span className="logo-sm">
-                            <img src={logo} alt="logo" height="30" />
-                        </span>
-                    </Link>
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const [dropdownOpenMobile, setDropdownOpenMobile] = useState(false)
 
-                    <Link to="/" className="logo logo-light">
-                        <span className="logo-sm">
-                            <img src={logo} alt="logo" height="30" />
-                        </span>
-                    </Link>
-                </div>
-                {/* end navbar-brand-box  */}
+  const toggle = () => setDropdownOpen(!dropdownOpen)
+  const toggleMobile = () => setDropdownOpenMobile(!dropdownOpenMobile)
 
-                {/* Start side-menu nav */}
-                <div className="flex-lg-column my-auto">
-                    <Nav className="side-menu-nav nav-pills justify-content-center" role="tablist">
-                        <NavItem id="profile">
-                            <NavLink id="pills-user-tab" className={classnames({ active: activeTab === 'profile' }) + " mb-2"} onClick={() => { toggleTab('profile'); }}>
-                                <i className="ri-user-2-line"></i>
-                            </NavLink>
-                        </NavItem>
-                        <UncontrolledTooltip target="profile" placement="top">
-                            プロフィール
-                        </UncontrolledTooltip>
-                        <NavItem id="Chats">
-                            <NavLink id="pills-chat-tab" className={classnames({ active: activeTab === 'chat' }) + " mb-2"} onClick={() => { toggleTab('chat'); }}>
-                                <i className="ri-message-3-line"></i>
-                            </NavLink>
-                        </NavItem>
-                        <UncontrolledTooltip target="Chats" placement="top">
-                            チャット
-                        </UncontrolledTooltip>
-                        <Dropdown nav isOpen={dropdownOpenMobile} toggle={toggleMobile} className="profile-user-dropdown d-inline-block d-lg-none dropup">
-                            <DropdownToggle nav>
-                                <img src={avatar1} alt="chatting system" className="profile-user rounded-circle" />
-                            </DropdownToggle>
-                            <DropdownMenu className="dropdown-menu-end">
-                                <DropdownItem onClick={() => { toggleTab('profile'); }}>Profile <i className="ri-profile-line float-end text-muted"></i></DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem href="/logout">Log out <i className="ri-logout-circle-r-line float-end text-muted"></i></DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                    </Nav>
-                </div>
-                {/* end side-menu nav */}
+  const toggleTab = (tab) => {
+    props.setActiveTab(tab)
+  }
 
-                <div className="flex-lg-column d-none d-lg-block">
-                    <Nav className="side-menu-nav justify-content-center">
-                        <li className="nav-item">
-                            <NavLink id="light-dark" className='mb-2' onClick={() => onChangeLayoutMode(mode)}>
-                                <i className="ri-sun-line theme-mode-icon"></i>
-                            </NavLink>
-                        </li>
-                        <Dropdown nav isOpen={dropdownOpen} className="nav-item btn-group dropup profile-user-dropdown" toggle={toggle}>
-                            <DropdownToggle className="nav-link mb-2" tag="a">
-                                <img src={avatar1} alt="" className="profile-user rounded-circle" />
-                            </DropdownToggle>
-                            <DropdownMenu>
-                                <DropdownItem onClick={() => { toggleTab('profile'); }}>Profile <i className="ri-profile-line float-end text-muted"></i></DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem href="/logout">Log out <i className="ri-logout-circle-r-line float-end text-muted"></i></DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                    </Nav>
-                </div>
-                {/* Side menu user */}
-            </div>
-        </React.Fragment>
-    );
+  const activeTab = props.activeTab
+
+  return (
+    <React.Fragment>
+      <div className='side-menu flex-lg-column me-lg-1'>
+        {/* LOGO */}
+        <div className='navbar-brand-box'>
+          <Link to='/' className='logo logo-dark'>
+            <span className='logo-sm'>
+              <img src={logo} alt='logo' height='30' />
+            </span>
+          </Link>
+
+          <Link to='/' className='logo logo-light'>
+            <span className='logo-sm'>
+              <img src={logo} alt='logo' height='30' />
+            </span>
+          </Link>
+        </div>
+        {/* end navbar-brand-box  */}
+
+        {/* Start side-menu nav */}
+        <div className='flex-lg-column my-auto'>
+          <Nav className='side-menu-nav nav-pills justify-content-center' role='tablist'>
+            <NavItem id='profile'>
+              <NavLink
+                id='pills-user-tab'
+                className={classnames({ active: activeTab === 'profile' }) + ' mb-2'}
+                onClick={() => {
+                  toggleTab('profile')
+                }}
+              >
+                <i className='ri-user-2-line'></i>
+              </NavLink>
+            </NavItem>
+            <UncontrolledTooltip target='profile' placement='top'>
+              プロフィール
+            </UncontrolledTooltip>
+            <NavItem id='Chats'>
+              <NavLink
+                id='pills-chat-tab'
+                className={classnames({ active: activeTab === 'chat' }) + ' mb-2'}
+                onClick={() => {
+                  toggleTab('chat')
+                }}
+              >
+                <i className='ri-message-3-line'></i>
+              </NavLink>
+            </NavItem>
+            <UncontrolledTooltip target='Chats' placement='top'>
+              チャット
+            </UncontrolledTooltip>
+            <Dropdown
+              nav
+              isOpen={dropdownOpenMobile}
+              toggle={toggleMobile}
+              className='profile-user-dropdown d-inline-block d-lg-none dropup'
+            >
+              <DropdownToggle nav>
+                <img src={avatar1} alt='chatting system' className='profile-user rounded-circle' />
+              </DropdownToggle>
+              <DropdownMenu className='dropdown-menu-end'>
+                <DropdownItem
+                  onClick={() => {
+                    toggleTab('profile')
+                  }}
+                >
+                  Profile <i className='ri-profile-line float-end text-muted'></i>
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem
+                  onClick={() => {
+                    logoutMutation.mutate()
+                  }}
+                >
+                  Log out <i className='ri-logout-circle-r-line float-end text-muted'></i>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Nav>
+        </div>
+        {/* end side-menu nav */}
+
+        <div className='flex-lg-column d-none d-lg-block'>
+          <Nav className='side-menu-nav justify-content-center'>
+            <li className='nav-item'>
+              <NavLink id='light-dark' className='mb-2' onClick={() => onChangeLayoutMode(mode)}>
+                <i className='ri-sun-line theme-mode-icon'></i>
+              </NavLink>
+            </li>
+            <Dropdown
+              nav
+              isOpen={dropdownOpen}
+              className='nav-item btn-group dropup profile-user-dropdown'
+              toggle={toggle}
+            >
+              <DropdownToggle className='nav-link mb-2' tag='a'>
+                <img src={avatar1} alt='' className='profile-user rounded-circle' />
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem
+                  onClick={() => {
+                    toggleTab('profile')
+                  }}
+                >
+                  Profile <i className='ri-profile-line float-end text-muted'></i>
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem
+                  onClick={() => {
+                    logoutMutation.mutate()
+                  }}
+                >
+                  Log out <i className='ri-logout-circle-r-line float-end text-muted'></i>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Nav>
+        </div>
+        {/* Side menu user */}
+      </div>
+    </React.Fragment>
+  )
 }
 
-const mapStatetoProps = state => {
-    return {
-        ...state.Layout
-    };
-};
+const mapStatetoProps = (state) => {
+  return {
+    ...state.Layout
+  }
+}
 
 export default connect(mapStatetoProps, {
-    setActiveTab
-})(LeftSidebarMenu);
+  setActiveTab
+})(LeftSidebarMenu)
