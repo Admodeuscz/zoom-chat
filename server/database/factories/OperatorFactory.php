d015f28a632f4fc3050a2355c89f1ad505c84794
Admodeuscz <work.doantv@gmail.com>
2025-01-21 18:40:25 +0700

    update: update create username in factory

--- server/database/factories/OperatorFactory.php
+++ server/database/factories/OperatorFactory.php
@@ -9,8 +9,11 @@ class OperatorFactory extends Factory
 {
     public function definition(): array
     {
+        $username = $this->faker->unique()->userName();
+        $filteredUsername = preg_replace('/[^a-zA-Z0-9_-]/', '', $username);
+
         return [
-            'op_id' => $this->faker->unique()->userName(),
+            'op_id' => $filteredUsername,
             'op_name' => $this->faker->name(),
             'op_name_kana' => $this->faker->name(),
             'pwd' => Hash::make('12345678'),
